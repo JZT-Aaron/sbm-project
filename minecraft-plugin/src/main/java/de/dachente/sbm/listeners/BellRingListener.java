8353a7f26b036bd2abff7e566e2a2f2cf862dad5
Aaron <aaron.guentner@outlook.de>
2026-03-18 19:52:13 +0100

    Add Doom-Bell

---
+++ minecraft-plugin/src/main/java/de/dachente/sbm/listeners/BellRingListener.java
@@ -0,0 +1,58 @@
+package de.dachente.sbm.listeners;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BellRingEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import de.dachente.sbm.main.Main;
+import de.dachente.sbm.managers.Info;
+import de.dachente.sbm.utils.Game;
+import de.dachente.sbm.utils.enums.Team;
+
+public class BellRingListener implements Listener {
+
+    @EventHandler
+    public void onRing(BellRingEvent event) {
+        Block bell = event.getBlock();
+        List<Location> bells = Main.parseList(Main.getPlugin().getConfig().getString("target.pos"));
+        if(!bells.contains(bell.getLocation())) return;
+        Game.pause();
+
+        int teamID = bells.indexOf(bell.getLocation());
+        Team team = teamID == 0 ? Team.BLUE : Team.RED;
+         
+        new BukkitRunnable() {
+            int count = 3;
+            public void run() {
+                for(Player all : Main.arena.getPlayers()) all.playSound(all.getLocation(), Sound.BLOCK_BELL_USE, 1.0f, 1.0f);
+                count--;
+                if(count <= 0) this.cancel();
+            }
+        }.runTaskTimer(Main.getPlugin(), 0, 20);
+        Info.showLangTitle("doom-bell", "%team%", team.getChatColor() + " @team." + team.getId());
+
+        new BukkitRunnable() {
+            public void run() {
+                for(String teamPlayerUUid : Game.getLivingPlayers(team)) {
+                    Player teamPlayer = Bukkit.getPlayer(UUID.fromString(teamPlayerUUid));
+                    if(teamPlayer == null) {
+                        Game.removeFromLivingPlayers(teamPlayerUUid);
+                        return;
+                    }
+                    Game.deadMode(teamPlayer);
+                }
+            }
+        }.runTaskLater(Main.getPlugin(), 80);
+        
+    }
+    
+}
--- minecraft-plugin/src/main/java/de/dachente/sbm/listeners/MoveListener.java
+++ minecraft-plugin/src/main/java/de/dachente/sbm/listeners/MoveListener.java
@@ -7,10 +7,16 @@ import de.dachente.sbm.utils.Game;
 import de.dachente.sbm.utils.StartClock;
 import de.dachente.sbm.utils.enums.GameState;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
 
 public class MoveListener implements Listener {
 
@@ -21,7 +27,52 @@ public class MoveListener implements Listener {
             Main.resendLobbySigns.remove(player);
             StartClock.updateSigns(player, true);
         }
-        if(Game.state().equals(GameState.RUNNING_REMATCH) && TeamManager.getTeamsPlayer().containsKey(player.getUniqueId().toString()) && player.getLocation().getY() < -30) {
+
+        if(Game.state().equals(GameState.RUNNING_MATCH)) {
+            for(int i = 0; i < 2; i++) {
+                List<Location> zonePoses = Main.parseList(Main.getPlugin().getConfig().getString("target.square"));
+                BoundingBox zone = BoundingBox.of(zonePoses.get(2*i), zonePoses.get((i*2)+1));
+
+                List<Location> poses = Main.parseList(Main.getPlugin().getConfig().getString("target.circle"));
+                Location von = event.getFrom();
+                Location zu = event.getTo();
+
+                if(von.getX() == zu.getX() && von.getZ() == von.getZ() || von.getY() > 7) return;
+
+                Location mitte = poses.get(i);
+
+                double dx = zu.getBlockX() - mitte.getBlockX();
+                double dz = zu.getBlockZ() - mitte.getBlockZ();
+
+                double distanzQuadrat = (dx * dx) + (dz * dz);
+
+                if (distanzQuadrat <= 20 || zone.contains(player.getLocation().toVector())) {
+                    double dxVon = von.getBlockX() - mitte.getBlockX();
+                    double dzVon = von.getBlockZ() - mitte.getBlockZ();
+                    double distanzVonQuadrat = (dxVon * dxVon) + (dzVon * dzVon);
+
+                    if (distanzQuadrat < 9 && distanzVonQuadrat >= 9) {
+                        von.setYaw(zu.getYaw());
+                        von.setPitch(zu.getPitch());
+                        von.setY(zu.getY());
+                        event.setTo(von);
+                    }
+                    Vector vector = new Vector(dx, 0, dz).normalize().multiply(0.5);
+                    vector.setY(0);
+                    Bukkit.getScheduler().runTask(Main.getPlugin(), () -> {
+                        player.setVelocity(vector);
+                    });
+                    break;
+                }
+            }
+            
+        }
+        
+        if((Game.state().equals(GameState.RUNNING_REMATCH) || Game.state().equals(GameState.PAUSED)) && Game.getLivingPlayers().containsKey(player.getUniqueId().toString()) && player.getLocation().getY() < -30) {
+            if(Game.state().equals(GameState.PAUSED))  {
+                player.teleport(Game.getRematchSpawnLocation(player));
+                return;
+            }
             Info.sendLangInfo("rematch.player-hit", "%player%", TeamManager.getTeam(player).getChatColor() + player.getName());
             if(player.getHealthScale() > 2) {
                 player.setHealthScale(player.getHealthScale()-2);
